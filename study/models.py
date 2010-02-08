d921d903166219eafe783f3e0cd74eaaa418943f
Leo Dirac <leo.dirac@gmail.com>
2010-02-07 20:14:53 -0800

    Picking lesson works.  Impressions include user.

--- study/models.py
+++ study/models.py
@@ -3,8 +3,7 @@ from django.contrib.auth.models import User
 from dbbpy.flashcards.models import Concept
 
 class Impression(models.Model):
-    #TODO: user should not actually be allowed to be null
-    user = models.ForeignKey(User, null=True)
+    user = models.ForeignKey(User)
     concept = models.ForeignKey(Concept)
     #TODO: add card instead of (as well as?) concept
     answered_date = models.DateTimeField(auto_now_add=True)
--- study/views.py
+++ study/views.py
@@ -7,24 +7,32 @@ from django.shortcuts import get_object_or_404
 from django.template import loader, Context, RequestContext
 from django.contrib.auth.decorators import login_required
 from dbbpy.flashcards.models import Concept
+from dbbpy.flashcards.models import Lesson
 from dbbpy.study.models import Impression
 
 @login_required
 def studyui(request):
     return render_to_response("study/studyui.html", context_instance=RequestContext(request))
 
-def pick_concept():
-    num = Concept.objects.count()
-    which = random.randint(1,num/10) # /10 to make it easier
-    #TODO: this won't work if the id space isn't dense
-    concept = get_object_or_404(Concept, pk=which)
-    return concept
+def pick_concept(request):
+    lesson = get_object_or_404(Lesson, pk=request.session['lesson'])
+    num = lesson.concepts.count()
+    which = random.randint(1,num)
+
+    # note: this loads the entire lesson, which is unnecessarily slow.
+    for concept in lesson.concepts.all():
+	which = which-1
+	if which==0:
+	    return concept
+
+    print "assertion fail. Should never get here in dbbpy.study.views.pick_concept"
+    return None
 
 
 def getqa(request):
     # pick a random card to show...
-    concept = pick_concept()
-    # TODO: make this less hacky
+    concept = pick_concept(request)
+    # TODO: generalize this for any lesson type
     q = concept.asset_set.get(asset_type=2).content
     a = concept.asset_set.get(asset_type=4).content
     data = { "question": q, "answer": a, "concept": concept.id }
@@ -39,10 +47,12 @@ def impression(request):
     print request.POST
     i.answer = request.POST['answer']
     i.concept_id = request.POST['concept']
+    i.user = request.user
     i.save()
     return HttpResponse("OK", mimetype='text/plain')
 
 @login_required
 def setlesson(request,lesson_id):
+    request.session['lesson'] = lesson_id
     return HttpResponseRedirect("/study/")
     
