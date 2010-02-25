c841b1d105ae24175c5f68d00c573da0aec7acca
Leo Dirac <leo.dirac@gmail.com>
2010-02-24 19:44:26 -0800

    better tests of flashcard fixtures

--- flashcards/tests.py
+++ flashcards/tests.py
@@ -1,36 +1,32 @@
-from dbbpy.flashcards.models import Lesson
-
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
-
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
-
+from dbbpy.flashcards.models import Lesson
 
 class FixtureTest(TestCase):
     """
     Test that the fixtures are reasonable.
+
+    Note that the fixtures have their unicode stripped out b/c
+    of django bugs -- it can't roundtrip through dumpdata, fixtures=
+    properly with unicode data. (loaddata works, but not fixtures=)
     """
     fixtures = ['vocab50.json']
 
-    def test_lessons(self):
-	# Test that there are lessons in the db
+
+    def test_basic_data(self):
+	"""
+	Check that the data loaded in the fixtures is structurally reasonable
+	"""
+
+	# Test that there are multiple lessons in the db
 	lessons = Lesson.objects.all()
-	self.failUnless(lessons.count() > 0)
+	self.failUnless(lessons.count() >= 1)
+	
+	# Look for a minimum number of concepts
+	lesson = lessons[0]
+	self.failUnless(lesson.concepts.count() > 5)
+
+	# Look inside the concept
+	concept = lesson.concepts.all()[0]
+	assets = concept.asset_set.all()
+	self.failUnless(assets.count() >= 2)
+
