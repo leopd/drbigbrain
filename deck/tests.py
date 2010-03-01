e4f4b9e606204fe66ec4ef33088667d284b0365b
Leo Dirac <leo.dirac@gmail.com>
2010-02-28 19:24:43 -0800

    basic fixture test in deck module

--- deck/tests.py
+++ deck/tests.py
@@ -1,24 +1,20 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from dbbpy.flashcards.models import Lesson
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+    fixtures = ['vocab50.json']
+    
+    def test_fixture_data(self):
+	"""
+	Check that the data loaded in the fixtures is structurally reasonable
+	"""
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
+	# Test that there are multiple lessons in the db
+	lessons = Lesson.objects.all()
+	self.failUnless(lessons.count() >= 1)
+	
+	# Look for a minimum number of concepts
+	lesson = lessons[0]
+	self.failUnless(lesson.concepts.count() > 5)
 
 
